aae2746d54a234205997be7ee96395b5364c0720
Hrutwik Dabhade <dabhadehrutwik@gmail.com>
2024-01-05 23:38:01 +0530

    new changes added

--- src/services/Api.js
+++ src/services/Api.js
@@ -5,13 +5,8 @@ import * as Constant from "./Constant"
 const ApiUrl = process.env.REACT_APP_API_KEY;
 
 const showRoomData = async () => {
-    try {
-        const result = await axios.get(ApiUrl + Constant.GET_ALL_ROOM);
-        return result.data
-    }
-    catch (error) {
-        alert(error.code)
-    }
+    const result = await axios.get(ApiUrl + Constant.GET_ALL_ROOM);
+    return result.data
 }
 
-export {showRoomData}
+export { showRoomData }
