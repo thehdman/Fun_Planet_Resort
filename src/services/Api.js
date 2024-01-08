05aad0050c741db9f7c3991e910b0cca58d65c32
Hrutwik Dabhade <dabhadehrutwik@gmail.com>
2024-01-08 10:58:51 +0530

    New changes added

--- src/services/Api.js
+++ src/services/Api.js
@@ -9,4 +9,44 @@ const showRoomData = async () => {
     return result.data
 }
 
-export { showRoomData }
+const showRoomTypeData = async () => {
+    const result = await axios.get(ApiUrl + Constant.GET_MASTER_BY_STATOUS_ROOM_TYPE);
+    return result.data
+}
+
+const showRoomStatus = async () => {
+    const result = await axios.get(ApiUrl + Constant.GET_MASTER_BY_STATOUS_ROOM_STATUS);
+    return result.data
+}
+
+const addRoomData = async (obj) => {
+    debugger;
+    try {
+        const result = await axios.post(ApiUrl + Constant.ADD_ROOM, obj);
+        return result.data;
+    }
+    catch (error) {
+        alert(error.code)
+    }
+}
+
+const editRoom = async (id) => {
+    try {
+        const result = await axios.get(ApiUrl + Constant.EDIT_ROOM + id);
+        return result.data;
+    }
+    catch (error) {
+        alert(error.code)
+    }
+}
+
+const deleteRoom = async (obj) => {
+    try {
+        const result = await axios.post(ApiUrl + Constant.DELETE_ROOM, obj);
+    }
+    catch (error) {
+        alert(error.code)
+    }
+}
+
+export { showRoomData, showRoomTypeData, showRoomStatus, addRoomData, editRoom, deleteRoom}
--- src/services/Constant.js
+++ src/services/Constant.js
@@ -1,1 +1,6 @@
 export const GET_ALL_ROOM = "GetAllRoom";
+export const GET_MASTER_BY_STATOUS_ROOM_TYPE = 'GetMasterByStatus?master=Room_Type';
+export const GET_MASTER_BY_STATOUS_ROOM_STATUS = "GetMasterByStatus?master=Room_Status";
+export const ADD_ROOM = "AddRoom";
+export const EDIT_ROOM = 'GetRoomById?id=';
+export const DELETE_ROOM = 'DeleteRoom';
