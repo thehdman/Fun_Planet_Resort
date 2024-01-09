5a648abf85ea123363b33383a78ff04aa1b74526
akanksha apugade <akankshaapugade3542@gmail.com>
2024-01-09 19:44:29 +0530

    Login page added

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Navbar from '../components/UI/Navbar'
 import RoomCreation from '../pages/RoomCreation';
+import Login from '../pages/Login';
+
 
 const AppRoutes = () => {
     return (
@@ -10,6 +12,9 @@ const AppRoutes = () => {
                 <Navbar></Navbar>
                 <Routes>
                     <Route path='RoomCreation' element={<RoomCreation></RoomCreation>}></Route>
+                    <Route path='Login' element={<Login></Login>}></Route>
+            
+
                 </Routes>
             </BrowserRouter>
         </div>
